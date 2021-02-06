d17ad19daf4894fedcd35060c78ab80d7ac64d42
Rodrigo Arce <albo@pragmore.com>
2021-02-06 12:46:50 -0300

    Remove cli-config

--- cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\EntityManager;
-use Fernet\Framework;
-
-$entityManager = Framework::getInstance()->getContainer()->get(EntityManager::class);
-ConsoleRunner::createHelperSet($entityManager);
