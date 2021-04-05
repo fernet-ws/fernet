f627f873e4e12764eb57c02189efde4168cb744e
Rodrigo Arce <albo@pragmore.com>
2021-04-05 07:32:46 -0300

    Remove session_start

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 require __DIR__.'/vendor/autoload.php';
 
-session_start();
-
 Dotenv\Dotenv::createImmutable(__DIR__)->load();
 Fernet\Framework::setUp([
     'rootPath' => __DIR__,
 ]);
+
+$faker = \Faker\Factory::create($_ENV['FAKER_LANG']);
+$faker->seed($_ENV['FAKER_SEED']);
+Fernet\Framework::getInstance()->getContainer()->add($faker::class, $faker);
